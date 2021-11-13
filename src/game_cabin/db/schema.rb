66f4f61ca66a1691f9d6c2a3cb69327b335141c8
bennychen2004181026 <ben83026@outlook.com>
2021-11-14 01:58:01 +1100

    Revert "Renew schema with products and categories table"
    This reverts commit 2077fc0fa854a3140fc526b29cb213256fd48757.

--- src/game_cabin/db/schema.rb
+++ src/game_cabin/db/schema.rb
@@ -10,35 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_13_075737) do
+ActiveRecord::Schema.define(version: 2021_11_12_013925) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "categories", force: :cascade do |t|
-    t.string "title"
-    t.integer "weight", default: 0
-    t.integer "products_quantity", default: 0
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["title"], name: "index_categories_on_title"
-  end
-
-  create_table "products", force: :cascade do |t|
-    t.bigint "category_id", null: false
-    t.string "title"
-    t.string "status", default: "Not available"
-    t.integer "amount", default: 0
-    t.string "uuid"
-    t.decimal "price", precision: 10, scale: 2
-    t.text "description"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["category_id"], name: "index_products_on_category_id"
-    t.index ["status", "category_id"], name: "index_products_on_status_and_category_id"
-    t.index ["title"], name: "index_products_on_title"
-    t.index ["uuid"], name: "index_products_on_uuid", unique: true
-  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -52,5 +27,4 @@ ActiveRecord::Schema.define(version: 2021_11_13_075737) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "products", "categories"
 end
