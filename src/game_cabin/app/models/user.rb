64706d1a6a52cdb793d2962c0af1f93732429731
bennychen2004181026 <ben83026@outlook.com>
2021-11-13 11:45:55 +1100

    add user attribute accessors and validations

--- src/game_cabin/app/models/user.rb
+++ src/game_cabin/app/models/user.rb
@@ -1,6 +1,31 @@
 class User < ApplicationRecord
+  # Provide access to password and comfirmation
+  attr_accessor :password, :password_comfirmation
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # validate the arributes
+  # For email
+  validates :email, presence: { message: "must be given please" } 
+  validates :email, uniqueness: { message: "already exists" }
+
+  # For password
+  validates :password, presence: { message: "must be given please" },
+  if: :need_validate_password
+  validates :password_comfirmation, presence: { message: "must be given please" },
+  if: :need_validate_password
+  validates :password, confirmation: { message: "must be consistent" },
+  if: :need_validate_password
+  validates :password, length: { minimum: 8, message: "minimum 8 characters" },
+  if: :need_validate_password
+
+  private
+  # helper method for when only changing the password
+  def need_validate_password
+    self.new_record? || (!self.password.nil? || !self.password_confirmation.nil?)
+  end
+
 end
