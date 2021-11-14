bd6a16aceeb60db2a30dbe9a7d2a52959c27e32f
bennychen2004181026 <77423140+bennychen2004181026@users.noreply.github.com>
2021-11-14 11:09:10 +1100

    Add ancestry gem for organizing category record

--- src/game_cabin/db/schema.rb
+++ src/game_cabin/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_13_075737) do
+ActiveRecord::Schema.define(version: 2021_11_13_235008) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2021_11_13_075737) do
     t.integer "products_quantity", default: 0
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "ancestry"
+    t.index ["ancestry"], name: "index_categories_on_ancestry"
     t.index ["title"], name: "index_categories_on_title"
   end
 
