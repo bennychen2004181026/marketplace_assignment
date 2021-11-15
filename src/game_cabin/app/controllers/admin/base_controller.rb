3b57a6498aa04701e031adeb5218850257c6233d
bennychen2004181026 <77423140+bennychen2004181026@users.noreply.github.com>
2021-11-15 11:12:50 +1100

    Build basic admin category management interface

--- src/game_cabin/app/controllers/admin/base_controller.rb
+++ src/game_cabin/app/controllers/admin/base_controller.rb
@@ -1,8 +1,6 @@
-# This controller is created only for admin dashboard
+# This controller is created only for admin
 class Admin::BaseController < ActionController::Base
-  before_action :authenticate_user!
-  # Just for making sure you can realize mistake in case you forgot to authorize your controller action
-  after_action :verify_authorized, except: [:show]
+
   #  Specifying Layouts for Admin BaseController
   layout 'admin/layouts/admin'
 
--- src/game_cabin/app/controllers/admin/categories_controller.rb
+++ src/game_cabin/app/controllers/admin/categories_controller.rb
@@ -1,4 +1,14 @@
-class Admin::CategoriesController < ApplicationController
+class Admin::CategoriesController < Admin::BaseController
   def index
+    # Classification with pagination and sort them in descending order
+    if params[:id].blank?
+      @categories = Category.roots
+    else
+      @category = Category.find(params[:id])
+      @categories = @category.children
+    end
+
+    @categories = @categories.page(params[:page] || 1).per_page(params[:per_page] || 10)
+      .order(id: "desc")
   end
 end
