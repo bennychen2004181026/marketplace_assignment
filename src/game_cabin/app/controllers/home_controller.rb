84389f79e9df1811b839048e0dd84f0361a3ef47
bennychen2004181026 <77423140+bennychen2004181026@users.noreply.github.com>
2021-11-18 15:31:57 +1100

    home page display listing categories and products

--- src/game_cabin/app/controllers/home_controller.rb
+++ src/game_cabin/app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
+    @categories = Category.grouped_data
+    @products = Product.available.page(params[:page] || 1).per_page(params[:per_page] || 12).order("id desc")
+    @users = User.all
   end
 end
--- src/game_cabin/config/routes.rb
+++ src/game_cabin/config/routes.rb
@@ -2,5 +2,17 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index'
-
+  
+  resources :users
+  resources :sessions
+  
+  namespace :admin do
+    root 'sessions#new'
+    resources :sessions
+    resources :categories
+    resources :products
+  end
+  
+  resources :categories, only: [:show]
+  resources :products, only: [:show]
 end
